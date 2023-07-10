4c006976d01be3e639099ba16e185fe5724dc892
tavantai <tavantai99dk@gmail.com>
2023-07-10 10:46:46 +0700

    a

--- w3-tuần 4_buổi3/bt_tuàn 4/style1.js
+++ w3-tuần 4_buổi3/bt_tuàn 4/style1.js
@@ -3,10 +3,11 @@ function loaddoc1(){
    html.onload=function(){
     if(this.readyState == 4 && this.status == 200){
         document.getElementById("demo1").innerHTML = this.responseText;
+        const data=JSON.parse(this.responseText);
+        return data.name;
     }
    }
    html.open("Get","https://raw.githubusercontent.com/openai/gym/master/README.md")
    html.send(); 
-
 }
 loaddoc1();
